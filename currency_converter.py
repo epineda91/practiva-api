5ededa66020c0a7caa99cda9ef492b7b86b44ef3
epineda91 <efsp91@gmail.com>
2024-08-17 18:12:27 -0600

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -1,15 +1,16 @@
 import requests
+from config import API_KEY
 
 class CurrencyConverter:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/"
+    def __init__(self):
+        self.api_key = API_KEY
+        self.url = f"https://v6.exchangerate-api.com/v6/{self.api_key}/latest/"
 
     def get_exchange_rate(self, from_currency, to_currency):
         response = requests.get(self.url + from_currency)
         data = response.json()
         if response.status_code == 200:
-            rates = data['conversion_rates']
+            rates = data["conversion_rates"]
             return rates.get(to_currency, None)
         else:
             print(f"Error: {data['error-type']}")
